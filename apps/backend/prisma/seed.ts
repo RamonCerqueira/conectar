53fabd2bf9507087f1f8865c7f89ae25b882ae73
alandas71 <alanalves.dev@gmail.com>
2026-08-07 16:18:22 -0300

    refactor: clean seed.ts imports

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -1,9 +1,3 @@
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
-dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
-
 import {
   PrismaClient,
   PerfilUsuario,
